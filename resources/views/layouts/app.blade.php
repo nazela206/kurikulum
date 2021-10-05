8e32791107b063dcc1b95b8fddc88e722da3470b
nazela206 <nazelaulhaqy.2019@student.uny.ac.id>
2021-10-05 21:40:28 +0700

    konfigurasi database dan tampilan home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,14 +51,14 @@
         <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown user-menu">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                    <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+                    <img src="https://e7.pngegg.com/pngimages/1010/156/png-clipart-textile-t-shirt-clothing-dress-logo-t-shirt-leaf-textile.png"
                          class="user-image img-circle elevation-2" alt="User Image">
                     <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                     <!-- User image -->
                     <li class="user-header bg-primary">
-                        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+                        <img src="https://e7.pngegg.com/pngimages/1010/156/png-clipart-textile-t-shirt-clothing-dress-logo-t-shirt-leaf-textile.png"
                              class="img-circle elevation-2"
                              alt="User Image">
                         <p>
